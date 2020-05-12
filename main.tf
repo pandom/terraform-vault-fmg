4d758d2900c624c96998c68f689656527ce098ac
Grant Orchard <go@hashicorp.com>
2020-05-12 14:09:47 +1000

    some issues reading remote state, wrote the variable into locals instead

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ data azurerm_subscription "this" {}
 
 # Reusable variables
 locals {
-  vault_url = data.terraform_remote_state.this.outputs.vault_url
+  vault_url = "http://azure-vault.go.hashidemos.io:8200"
   users = {
     "grant":"go@hashicorp.com",
     "burkey":"burkey@hashicorp.com"
